266143204089b42692e2c2675f2159e2ee93b706
Nazmul Karim Chowdhury <nkchowdhury627@gmail.com>
2021-01-29 13:20:53 -0800

    new add

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
 document.getElementById('ticket-incrase').addEventListener('click',function(){
-    
-    const ticketInput = document.getElementById('ticket-count');
-    const ticketCount = parseInt(ticketInput.value);
-    const ticketNewCount = ticketCount + 1;
-    ticketInput.value = ticketNewCount;
 
+    handleTricketChange(true)
 
 })
 
 document.getElementById('ticket-decrase').addEventListener('click', function(){
+    handleTricketChange(false)
+
+})
+
+function handleTricketChange(isIncrase){
 
     const ticketInput = document.getElementById('ticket-count');
     const ticketCount = parseInt(ticketInput.value);
-    const ticketNewCount = ticketCount - 1;
+    let ticketNewCount = ticketCount;
+    if(isIncrase == true ){
+        ticketNewCount = ticketCount + 1;
+    }
+    if(isIncrase == false && ticketCount > 0){
+    ticketNewCount = ticketCount - 1;
+    }
     ticketInput.value = ticketNewCount;
+    const ticketTotal = ticketNewCount * 100;
+}
 
-})
+
+
+
+// document.getElementById('ticket-incrase').addEventListener('click',function(){
+    
+//     const ticketInput = document.getElementById('ticket-count');
+//     const ticketCount = parseInt(ticketInput.value);
+//     const ticketNewCount = ticketCount + 1;
+//     ticketInput.value = ticketNewCount;
+//     const ticketTotal = ticketNewCount * 100;
+
+
+// })
+
+// document.getElementById('ticket-decrase').addEventListener('click', function(){
+
+//     const ticketInput = document.getElementById('ticket-count');
+//     const ticketCount = parseInt(ticketInput.value);
+//     const ticketNewCount = ticketCount - 1;
+//     ticketInput.value = ticketNewCount;
+//     const ticketTotal = ticketNewCount * 100;
+
+// })
